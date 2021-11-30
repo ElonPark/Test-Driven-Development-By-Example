5f9f75cedd3512398819226de958d355918fb6c4
Elon Park <dev.sungwoon@gmail.com>
2021-11-30 19:27:31 +0900

    fix: fix Money.equals when sub class equality test

--- Chapter01/Money/src/test/kotlin/MoneyTests.kt
+++ Chapter01/Money/src/test/kotlin/MoneyTests.kt
@@ -23,5 +23,6 @@ class MoneyTests {
         assertFalse(Dollar(5).equals(Dollar(6)))
         assertTrue(Franc(5).equals(Franc(5)))
         assertFalse(Franc(5).equals(Franc(6)))
+        assertFalse(Franc(5).equals(Dollar(5)))
     }
 }
