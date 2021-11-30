421daba590850f59a74f71f93815b22803a14e9e
Elon Park <dev.sungwoon@gmail.com>
2021-12-01 00:51:02 +0900

    feat: add Sum

--- Chapter01/Money/src/main/kotlin/Money.kt
+++ Chapter01/Money/src/main/kotlin/Money.kt
@@ -1,4 +1,4 @@
-class Money(protected var amount: Int, val currency: String): Expression {
+class Money(internal val amount: Int, val currency: String): Expression {
 
     companion object {
         fun dollar(amount: Int): Money = Money(amount, "USD")
@@ -6,15 +6,17 @@ class Money(protected var amount: Int, val currency: String): Expression {
     }
 
     override fun equals(other: Any?): Boolean {
-        val money = other as Money
-        return amount == money.amount && currency.equals(money.currency)
+        if (other !is Money) return false
+        return amount == other.amount && currency.equals(other.currency)
     }
 
     override fun toString(): String = "$amount $currency"
 
+    override fun reduce(to: String): Money = this
+
     fun times(multiplier: Int): Money? = Money(amount * multiplier, currency)
 
     fun plus(addend: Money): Expression {
-        return Money(amount + addend.amount, currency)
+        return Sum(this, addend)
     }
 }
