bac894c7442881677ebc3c9cf8ace7abc1bb1e0b
Elon Park <dev.sungwoon@gmail.com>
2021-12-04 21:06:51 +0900

    feat: ch02-18

--- Chapter02/TestCase/testcase/WasRun.py
+++ Chapter02/TestCase/testcase/WasRun.py
@@ -1,2 +1,11 @@
-class WasRun:
-    pass
+from testcase.TestCase import TestCase
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.was_run = None
+        TestCase.__init__(self, name)
+
+
+    def test_method(self):
+        self.was_run = 1
